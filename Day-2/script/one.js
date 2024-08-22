955c6c275dcc394c6aceee40fdbb540e697afb90
atheist31 <rkb0808@gmaol.com>
2024-08-22 15:09:56 +0530

    array type

--- Day-2/script/one.js
+++ Day-2/script/one.js
@@ -1,3 +1,6 @@
 "use strict";
-let message = "Welcome to your life!!!";
-alert(message);
+// let message:(string | number) = "Welcome to your life!!!";
+// alert(message);
+// let message:string[] = ["welcome to your life","Enjoy your everyday"];
+let message = ["welcome to your life", "Enjoy your everyday"];
+alert(message[Math.floor(Math.random() * message.length - 1)]);
--- Day-2/src/one.ts
+++ Day-2/src/one.ts
@@ -1,2 +1,7 @@
-let message = "Welcome to your life!!!";
-alert(message);
+// let message:(string | number) = "Welcome to your life!!!";
+// alert(message);
+
+
+// let message:string[] = ["welcome to your life","Enjoy your everyday"];
+let message:Array<string> = ["welcome to your life","Enjoy your everyday"];
+alert(message[Math.floor(Math.random()*message.length-1)])
