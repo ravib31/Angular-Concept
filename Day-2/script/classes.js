95bf38918511492ed7f5f8140dc349e4c2d87168
ravib31 <rkb0808@gmail.com>
2024-08-22 20:14:35 +0530

    using super

--- Day-2/script/classes.js
+++ Day-2/script/classes.js
@@ -1,21 +1,60 @@
 "use strict";
-class Hero {
-    constructor() {
-        this.title = "Batman";
-        this._firstName = "Bruce";
-        this._lastName = "Wayne";
+// class Hero{
+//     public title:string="Batman";
+//     private _firstName:string="Bruce";
+//     private _lastName:string="Wayne";
+//     static version:number= 1001;
+//     constructor(ntitle:string,nfirstname:string,nlastname:string){
+//         this.title=ntitle;
+//         this._firstName = nfirstname;
+//         this._lastName = nlastname
+// }
+//     get firstname(){
+//         return this._firstName;
+//     }
+//     set firstname(nfirstname:string){
+//         this._firstName=nfirstname;
+//     }
+//     get lastname(){
+//         return this._lastName;
+//     }
+//     set lastname(nlastname:string){
+//         this._lastName = nlastname;
+//     }
+// }
+// let hero = new Hero("spiderman","peter","parker");
+// console.log(hero.title);
+// console.log(hero.firstname);
+class Person {
+    constructor(canwalk) {
+        this.canwalk = canwalk;
+    }
+}
+class Hero extends Person {
+    constructor(title, _firstname, _lastname, ncw) {
+        super(ncw);
+        this.title = title;
+        this._firstname = _firstname;
+        this._lastname = _lastname;
+    }
+    fullname() {
+        return this._firstname + " " + this._lastname;
     }
     get firstname() {
-        return this._firstName;
+        return this._firstname;
     }
     set firstname(nfirstname) {
-        this._firstName = nfirstname;
+        this._firstname = nfirstname;
     }
     get lastname() {
-        return this._lastName;
+        return this._lastname;
     }
     set lastname(nlastname) {
-        this._lastName = nlastname;
+        this._lastname = nlastname;
     }
 }
 Hero.version = 1001;
+let hero = new Hero("spiderman", "peter", "parker", "I can fly");
+console.log(hero.title);
+console.log(hero.firstname);
+console.log(hero.canwalk);
--- Day-2/src/classes.ts
+++ Day-2/src/classes.ts
@@ -1,29 +1,72 @@
-class Hero{
-    public title:string="Batman";
-    private _firstName:string="Bruce";
-    private _lastName:string="Wayne";
-    static version:number= 1001;
+// class Hero{
+//     public title:string="Batman";
+//     private _firstName:string="Bruce";
+//     private _lastName:string="Wayne";
+//     static version:number= 1001;
 
-    constructor(ntitle:string,nfirstname:string,nlastname:string){
-        this.title=ntitle;
-        this._firstName = nfirstname;
-        this._lastName = nlastname
+//     constructor(ntitle:string,nfirstname:string,nlastname:string){
+//         this.title=ntitle;
+//         this._firstName = nfirstname;
+//         this._lastName = nlastname
+// }
+
+//     get firstname(){
+//         return this._firstName;
+//     }
+//     set firstname(nfirstname:string){
+//         this._firstName=nfirstname;
+//     }
+//     get lastname(){
+//         return this._lastName;
+//     }
+//     set lastname(nlastname:string){
+//         this._lastName = nlastname;
+//     }
+// }
+
+// let hero = new Hero("spiderman","peter","parker");
+// console.log(hero.title);
+// console.log(hero.firstname);
+
+class Person {
+  constructor(public canwalk: string) {}
+}
+interface IHero {
+  title: string;
+  firstname: string;
+  lastname: string;
+  fullname(): string;
 }
 
-    get firstname(){
-        return this._firstName;
-    }
-    set firstname(nfirstname:string){
-        this._firstName=nfirstname;
-    }
-    get lastname(){
-        return this._lastName;
-    }
-    set lastname(nlastname:string){
-        this._lastName = nlastname;
-    }
+class Hero extends Person implements IHero {
+  static version: number = 1001;
+
+  constructor(
+    public title: string,
+    private _firstname: string,
+    private _lastname: string,
+    ncw: string
+  ) {
+    super(ncw);
+  }
+  fullname() {
+    return this._firstname + " " + this._lastname;
+  }
+  get firstname() {
+    return this._firstname;
+  }
+  set firstname(nfirstname: string) {
+    this._firstname = nfirstname;
+  }
+  get lastname() {
+    return this._lastname;
+  }
+  set lastname(nlastname: string) {
+    this._lastname = nlastname;
+  }
 }
 
-let hero = new Hero("spiderman","peter","parker");
+let hero = new Hero("spiderman", "peter", "parker", "I can fly");
 console.log(hero.title);
 console.log(hero.firstname);
+console.log(hero.canwalk);
